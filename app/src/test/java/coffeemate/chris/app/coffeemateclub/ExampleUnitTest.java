1120ccc2835da2790be48da8809d564e57b06477
Chris Maher <chrismaher.wit@gmail.com>
2016-02-26 00:52:19 +0000

    Start Brewing...

---
+++ app/src/test/java/coffeemate/chris/app/coffeemateclub/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package coffeemate.chris.app.coffeemateclub;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
